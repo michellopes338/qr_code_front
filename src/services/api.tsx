b62a7b4c7d0c5298de344554207a800eda6ee55d
Michel Lopes <lopesmichel489@gmail.com>
2024-05-21 20:43:24 -0300

    added Accept header on api service

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -17,7 +17,7 @@ export const api = axios.create({
     // baseURL: 'http://localhost:3000',
     timeout: 1000,
     headers: {
-        "Content-Type": 'application/json'
+        "Accept": 'application/json'
     }
 })
 
