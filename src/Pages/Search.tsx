7341ccbb8a558e3afeceb5d628923012479ec59f
Michel Lopes Cruz <lopesmichel489@gmail.com>
2025-02-19 19:59:26 +0000

    removed unused functions

--- src/Pages/Search.tsx
+++ src/Pages/Search.tsx
@@ -1,16 +1,9 @@
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import { api } from "../services/api";
 
-interface Nomes {
-  nome: string;
-  matricula: string;
-}
 
 export default function Search() {
   const navigate = useNavigate();
-  const [search_names, setSearch_nomes] = useState<Array<Nomes> | null>(null);
-  const [is_autocomplete_hidden, setIs_autocomplete_hidden] = useState<boolean>(true);
   const search_input_ref = useRef<HTMLInputElement>(null);
 
   function to_show_page(e: React.FormEvent<HTMLFormElement>) {
@@ -21,33 +14,9 @@ export default function Search() {
     navigate(`/${treinamentos_from}`);
   }
 
-  async function autocomplete(event: React.ChangeEvent<HTMLInputElement>) {
-    const regex = /^[a-zA-Z]+$/;
-    const value = event.target.value;
-    const is_chapa = regex.test(value[0]); // test if first char of input value is numeric
-
-    if (!is_chapa) {
-      return;
-    }
-
-    setIs_autocomplete_hidden(false);
-
-    const response = await api.get(`/autocomplete/${value}`);
-    const names = response.data;
-    setSearch_nomes(names);
-  }
-
-  function hide_autocomplete() {
-    setIs_autocomplete_hidden(true);
-  }
-
-  function select_option(selected: string) {
-    search_input_ref.current!.value = selected;
-  }
-
   return (
     <>
-      <div className="h-screen flex items-center justify-center" onClick={hide_autocomplete}>
+      <div className="h-screen flex items-center justify-center">
         <form onSubmit={to_show_page}>
           <div>
             <label className="block text-gray-700 font-bold" htmlFor="search">
@@ -58,7 +27,6 @@ export default function Search() {
             <input
               name='search'
               ref={search_input_ref}
-              onChange={autocomplete}
               id="search"
               autoComplete='off'
               placeholder='3024...'
@@ -86,14 +54,6 @@ export default function Search() {
                     ></path>
                   </svg>
                 </button>
-              </div>
-
-              <div className="w-full h-60 border border-gray-300 rounded-md bg-white absolute overflow-y-auto" hidden={is_autocomplete_hidden}>
-                {search_names?.map((obj) => {
-                  return (
-                    <div onClick={() => {select_option(obj.nome)}} className="px-5 border-b border-gray-200 text-stone-900 cursor-pointer hover:bg-slate-100" key={obj.matricula}>{obj.nome}</div>
-                  )
-                })}
               </div>
           </div>
         </form>
